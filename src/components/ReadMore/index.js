a1969363021e3c8334cb0d3a3dc5847d079d4d17
Shiori030 <nnn666nnn4125@gmail.com>
2024-12-11 12:00:28 +0800

    refactor: 更改行數邏輯跟css附加邏輯

--- src/components/ReadMore/index.js
+++ src/components/ReadMore/index.js
@@ -4,18 +4,13 @@ import { useState, useEffect, useRef } from 'react'
 export default function ReadMore({ title, content, lines }) {
   const [isOpen, setIsOpen] = useState(false)
   const [isMore, setIsMore] = useState(false)
-  const [isLoading,setIsLoading] = useState(true);
-  const [totalLines, setTotalLines] = useState(0);
+  const [isLoading,setIsLoading] = useState(true)
+  const [totalLines, setTotalLines] = useState(0)
   const contentRef = useRef()
 
 
   useEffect(() => {
-    const contentDOM = contentRef.current;
-    const range = document.createRange();
-    range.selectNodeContents(contentDOM);
-    const rects = Array.from(range.getClientRects())
-    setTotalLines(rects.length);
-
+    setTotalLines(contentRef.current.getClientRects().length)
   }, [content,lines])
 
   useEffect(() => {
@@ -24,12 +19,11 @@ export default function ReadMore({ title, content, lines }) {
     }
 
     if (totalLines > lines) {
-      setIsMore(true);
+      setIsMore(true)
     }
 
     setIsLoading(false)
   }, [totalLines,lines])
-
 
   return (
     <div className={`${styles.readmore}`} style={{opacity: isLoading ? 0 : 1}}>
@@ -37,11 +31,13 @@ export default function ReadMore({ title, content, lines }) {
         <h2 className={`${styles.title}`}>{title}</h2>
       </div>
       <div className={`${styles.contentArea}`}>
-        <p className={`${styles.content}`} style={{ WebkitLineClamp: isOpen ? null : lines }} ref={contentRef}>{content}</p>
+        <span className={isMore ? `${styles.content}` : null} style={{ WebkitLineClamp: isOpen ? null : lines }} ref={contentRef}>{content}</span>
       </div>
-      {isMore ? (<div className={`${styles.buttonArea}`}>
-        <button onClick={() => setIsOpen(!isOpen)} className={`${styles.button}`}>{isOpen ? "Read less" : "Read more"}</button>
-      </div>) : null}
+      {isMore ? (
+        <div className={`${styles.buttonArea}`}>
+          <button onClick={() => setIsOpen(!isOpen)} className={`${styles.button}`}>{isOpen ? "Read less" : "Read more"}</button>
+        </div>
+      ) : null}
     </div>
   )
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,14 +5,14 @@ import { newsDatas } from "./newsDatas"
 export default function ReadMoreTest() {
   return (
     <div>
-      {newsDatas.map((data) => (
-        <div className={`${styles.readmoreArea}`}>
-        <ReadMore
-          title={data.title}
-          content={data.content}
-          lines={data.lineLimit}
-        />
-      </div>
+      {newsDatas.map((data, index) => (
+        <div className={`${styles.readmoreArea}`} key={`read_more_${index}`}>
+          <ReadMore
+            title={data.title}
+            content={data.content}
+            lines={data.lineLimit}
+          />
+        </div>
       ))}
     </div>
   )
